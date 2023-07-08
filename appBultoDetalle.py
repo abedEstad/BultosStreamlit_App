2a937588a042a15d38b232cb60318b8d38611ea0
abedEstad <72113833+abedEstad@users.noreply.github.com>
2023-07-08 14:12:19 -0500

    Update appBultoDetalle.py

--- appBultoDetalle.py
+++ appBultoDetalle.py
@@ -16,6 +16,10 @@ import gspread
 from google.oauth2 import service_account
 from google.oauth2.service_account import Credentials
 
+import io
+from io import BytesIO
+buffer = io.BytesIO()
+
 st.set_page_config(page_title='***TSC - APLICACIONES WEB***',page_icon='🤡',layout='wide')
 st.title(':sunglasses: :sun_with_face: :face_with_cowboy_hat: :green[Creación de Bultos por Machine Learning] :sunglasses: :sun_with_face: :face_with_cowboy_hat:')
 st.write('_Esta es una version de app que permite subir un archivo excel, editarlo, guardarlo y exportarlo a tu directorio. EXCEL XLSX!_ :sunglasses:')
